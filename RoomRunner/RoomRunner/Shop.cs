9517a0ae7a79e1ffbb5f04aa37baf7dabbbff29b
Owen Isenhart <isenhart.owen@gmail.com>
2023-02-10 11:36:35 -0600

    Made a basic shopItem class and shop class

---
+++ RoomRunner/RoomRunner/Shop.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoomRunner
+{
+    class Shop
+    {
+        public List<ShopItem> items;
+        public Shop(List<ShopItem> itemList)
+        {
+            items = itemList;
+        }
+        public void Buy(ShopItem item, Player jeb)
+        {
+            //if jeb's money is greater than or equal to the items price
+            //add it to players item list and subtract their money
+        }
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            //will make this in a 2d grid style, just can't do it without being able to test what it looks like
+            for (int i = 0; i < items.Count; i++)
+            {
+                spriteBatch.Draw(items[i].tex, items[i].rect, Color.White);
+            }
+        }
+    }
+}
---
+++ RoomRunner/RoomRunner/ShopItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoomRunner
+{
+    class ShopItem
+    {
+        public int price;
+        public string name;
+        public Rectangle rect;
+        public Texture2D tex;
+        public ShopItem(int p, string n, Rectangle r, Texture2D t)
+        {
+            price = p;
+            name = n;
+            rect = r;
+            tex = t;
+        }
+    }
+}
