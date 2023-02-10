12491b8d0050fce423095aaa5936aa54f3c8f21f
Samuel <samuel.just.917@k12.friscoisd.org>
2023-02-10 11:24:40 -0600

    Basic Player class

---
+++ RoomRunner/RoomRunner/Player.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoomRunner
+{
+    class Player
+    {//d
+        public bool IsAlive;//eeee
+        public Vector2 Velocity, Position;
+
+        public Player()
+        {
+            IsAlive = true;
+        }
+    }
+}
