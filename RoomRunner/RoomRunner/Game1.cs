5c89acc0fbaeb8407dd9564ba627d3e89920a168
Christian Zech <christian.zech.633@k12.friscoisd.org>
2023-02-14 11:25:05 -0600

    Made room class with addanimationtexture and draw methods.

--- RoomRunner/RoomRunner/Game1.cs
+++ RoomRunner/RoomRunner/Game1.cs
@@ -33,6 +33,8 @@ namespace RoomRunner
         Rectangle window;
 
 
+
+
         int gameTimer;
 
 
---
+++ RoomRunner/RoomRunner/Room.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoomRunner
+{
+    class Room
+    {
+
+
+        public List<Texture2D> animationTextures;
+        public Texture2D background;
+
+
+
+        public Room(Texture2D background)
+        {
+            this.background = background;
+            animationTextures = new List<Texture2D>();
+
+
+
+
+
+        }
+
+        public void AddAnimationTexture(Texture2D texture)
+        {
+
+        }
+
+        public void Draw(int gameTimer)
+        {
+
+            
+
+
+        }
+
+
+    }
+}
