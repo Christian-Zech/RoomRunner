dfdf7b1ea21ac74bb4eeb099c03e7768e887c83d
Owen Isenhart <isenhart.owen@gmail.com>
2023-02-22 11:20:24 -0600

    added content

--- RoomRunner/RoomRunner/Game1.cs
+++ RoomRunner/RoomRunner/Game1.cs
@@ -284,7 +284,7 @@ namespace RoomRunner
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.Gray);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
