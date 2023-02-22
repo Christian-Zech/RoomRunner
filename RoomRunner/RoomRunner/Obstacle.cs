4221af8c05d5a2463f0e15257da365c9498c9d22
samuel.just.917@k12.friscoisd.org <samuel.just.917@k12.friscoisd.org>
2023-02-22 12:13:23 -0600

    Made obstacle progress

--- RoomRunner/RoomRunner/Obstacle.cs
+++ RoomRunner/RoomRunner/Obstacle.cs
@@ -9,19 +9,14 @@ namespace RoomRunner
 {
     public class Obstacle : Animation
     {
-        public static string[] States => statesstates;
-        private static readonly string[] statesstates = new string[] { "Idle" }; //NEVER USE THIS VARIABLE, ONLY USE THE UPPERCASE ONE!!!!
-
-        private Texture2D texture;
         public Rectangle BoundingBox;
-        private int speed, timeUntilShown;
+        private int speed;
 
-        public Obstacle(Texture2D txt, Rectangle bounding, int speed, int timeUntilShown) : base(States)
+        public Obstacle(Texture2D[] animation, Rectangle bounding, int speed, int animSpeed = 15) : base(new string[] { "Idle" })
         {
-            texture = txt;
             BoundingBox = bounding;
             this.speed = speed;
-            this.timeUntilShown = timeUntilShown;
+            AddAnimation("Idle", animSpeed, animation);
         }
 
         public new void Update()
@@ -32,8 +27,21 @@ namespace RoomRunner
         }
         public void Draw(SpriteBatch sb)
         {
-            sb.Draw(texture, BoundingBox, Color.White);
+            sb.Draw(CurrentTexture, BoundingBox, Color.White);
         }
         public bool DoesCollideWith(Rectangle other) => BoundingBox.Intersects(other);
+
+        public static Obstacle GenerateObstacle(Random rdm = default)
+        {
+            if (rdm == default) rdm = new Random(4298523);
+            return null;
+        }
+        public static void MakeObstacles(Texture2D enemySpriteSheet, GraphicsDevice gd)
+        {
+            List<Texture2D[]> obstacles = new List<Texture2D[]>();
+            Rectangle[] rects = Player.LoadSheet(4, 5, 32, 32);
+
+            //obstacles.Add()
+        }
     }
 }
