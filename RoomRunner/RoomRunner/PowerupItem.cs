16c3daa2b6da473378f81f74123f57471e31966c
Owen Isenhart <isenhart.owen@gmail.com>
2023-02-28 12:11:31 -0600

    made some foundations for the two classes needed, much will need to be changed

---
+++ RoomRunner/RoomRunner/PowerupItem.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoomRunner
+{
+    
+    class PowerupItem
+    {
+        public string id;
+        public int duration;
+        public bool active;
+        public PowerupItem(string name, int duration, bool active)
+        {
+            id = name;
+            this.duration = duration;
+            this.active = active;
+        }
+        public void Activate()
+        {
+            if (active = true)
+                duration--;
+            else
+                active = true;
+
+            if (duration <= 0)
+                active = false;
+        }
+    }
+}
---
+++ RoomRunner/RoomRunner/Powerups.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RoomRunner
+{
+    class Powerups
+    {
+        List<PowerupItem> powerups;
+        List<int> quantities;
+
+        public Powerups()
+        {
+            powerups = new List<PowerupItem> { new PowerupItem("Time Control", 10, false), new PowerupItem("Can't Die", 10, false), new PowerupItem("Instakill", 1, false), new PowerupItem("Magnet", 10, false) };
+            quantities = new List<int> { 0, 0, 0, 0 };
+        }
+        public void AddPowerup(int index)
+        {
+            quantities[index]++;
+        }
+        public void UsePowerup(int index)
+        {
+            if (quantities[index] > 0 && !ActivePowerups())
+            {
+                quantities[index]--;
+                powerups[index].Activate();
+            }
+        }
+
+        public bool ActivePowerups()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (powerups[i].active)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
