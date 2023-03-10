cf3014b61c9911c336d86a45610addd1fe74c2b4
Owen Isenhart <isenhart.owen@gmail.com>
2023-03-10 11:08:02 -0600

    added animation

--- RoomRunner/RoomRunner/PowerupItem.cs
+++ RoomRunner/RoomRunner/PowerupItem.cs
@@ -18,12 +18,14 @@ namespace RoomRunner
         public int duration;
         int durationTemp;
         public bool active;
+        public int count;
         public PowerupItem(string name, int duration, bool active)
         {
             id = name;
             this.duration = duration;
             durationTemp = duration;
             this.active = active;
+            count = 0;
         }
         public void Activate()
         {
@@ -38,6 +40,36 @@ namespace RoomRunner
                 duration = durationTemp;
             }
                 
+        }
+        public double AnimateLinear(List<Rectangle> frames, double currentFrameIndex)
+        {
+            currentFrameIndex += 0.09;
+            if (currentFrameIndex > frames.Count)
+            {
+                currentFrameIndex = 0;
+            }
+            return currentFrameIndex;
+        }
+        public double AnimateReverse(List<Rectangle> frames, double currentFrameIndex)
+        {
+            if (count == 0)
+            {
+                currentFrameIndex += 0.09;
+                if (currentFrameIndex + .6 > frames.Count)
+                {
+                    count = 1;
+                }
+            }
+            else if (count == 1)
+            {
+                currentFrameIndex -= 0.09;
+                if (currentFrameIndex - .6 < 0)
+                {
+                    count = 0;
+                }
+            }
+            return currentFrameIndex;
+
         }
     }
 }
--- RoomRunner/RoomRunner/Powerups.cs
+++ RoomRunner/RoomRunner/Powerups.cs
@@ -13,13 +13,17 @@ namespace RoomRunner
 {
     class Powerups
     {
+        int count1, count2, count3, count4;
         public List<PowerupItem> items;
         public List<int> quantities;
-
+        public double currentFrameIndex1, currentFrameIndex2, currentFrameIndex3, currentFrameIndex4;
+        int count;
         public Powerups()
         {
             items = new List<PowerupItem> { new PowerupItem("Time Control", 400, false), new PowerupItem("Can't Die", 600, false), new PowerupItem("Instakill", 10, false), new PowerupItem("Magnet", 600, false) };
             quantities = new List<int> { 1, 1, 1, 1 };
+            currentFrameIndex1 = currentFrameIndex2 = currentFrameIndex3 = currentFrameIndex4 = 0;
+            count = count1 = count2 = count3 = count4 = 0;
         }
         public void AddPowerup(int index)
         {
@@ -70,18 +74,25 @@ namespace RoomRunner
             }
         }
         
-        public void Draw(SpriteBatch spriteBatch, Texture2D textures, Texture2D pixel, List<Rectangle> clock, List<Rectangle> skull, List<Rectangle> nuke, List<Rectangle> magnet, SpriteFont font, Color color)
+        public void Draw(SpriteBatch spriteBatch, Texture2D textures, Texture2D pixel, List<Rectangle> clock, List<Rectangle> skull, List<Rectangle> nuke, List<Rectangle> magnet, SpriteFont font, SpriteFont font2, Color color)
         {
-            spriteBatch.Draw(pixel, new Rectangle(40, 20, 390, 130), Color.Gray*.1f);
-            color = new Color(255 - color.R, 255 - color.G, 255 - color.B); //inverted font color so hopefully easy to read on every background
-            spriteBatch.DrawString(font, "" + quantities[0], new Vector2(75, 120), color);
-            spriteBatch.DrawString(font, "" + quantities[1], new Vector2(175, 120), color);
-            spriteBatch.DrawString(font, "" + quantities[2], new Vector2(275, 120), color);
-            spriteBatch.DrawString(font, "" + quantities[3], new Vector2(375, 120), color);
-            spriteBatch.Draw(textures, new Rectangle(50, 40, 70, 70), clock[0], Color.White);
-            spriteBatch.Draw(textures, new Rectangle(150, 40, 70, 70), skull[0], Color.White);
-            spriteBatch.Draw(textures, new Rectangle(250, 40, 70, 70), nuke[0], Color.White);
-            spriteBatch.Draw(textures, new Rectangle(350, 40, 70, 70), magnet[0], Color.White);
+            currentFrameIndex1 = items[0].AnimateLinear(clock, currentFrameIndex1);
+            currentFrameIndex2 = items[1].AnimateReverse(skull, currentFrameIndex2);
+            currentFrameIndex3 = items[2].AnimateLinear(nuke, currentFrameIndex3);
+            currentFrameIndex4 = items[3].AnimateLinear(magnet, currentFrameIndex4);
+
+            spriteBatch.Draw(pixel, new Rectangle(40, 20, 390, 130), Color.Black*.3f);
+            color = new Color(255 - color.R, 255 - color.G, 255 - color.B); 
+            spriteBatch.DrawString(font, "" + quantities[0], new Vector2(75, 120), Color.White);
+            spriteBatch.DrawString(font, "" + quantities[1], new Vector2(175, 120), Color.White);
+            spriteBatch.DrawString(font, "" + quantities[2], new Vector2(275, 120), Color.White);
+            spriteBatch.DrawString(font, "" + quantities[3], new Vector2(375, 120), Color.White);
+            
+            
+            spriteBatch.Draw(textures, new Rectangle(50, 40, 70, 70), clock[(int)(currentFrameIndex1)], Color.White);
+            spriteBatch.Draw(textures, new Rectangle(150, 40, 70, 70), skull[(int)(currentFrameIndex2)], Color.White);
+            spriteBatch.Draw(textures, new Rectangle(250, 40, 70, 70), nuke[(int)(currentFrameIndex3)], Color.White);
+            spriteBatch.Draw(textures, new Rectangle(350, 40, 70, 70), magnet[(int)(currentFrameIndex4)], Color.White);
         }
     }
 }
