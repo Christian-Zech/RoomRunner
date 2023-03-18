800c0d42c6abc9059a71d26e6a9fbeb30324ae0e
Christian <christian.zech.633@k12.friscoisd.org>
2023-03-18 14:29:45 -0500

    Added coin class.

---
+++ RoomRunner/RoomRunner/Coin.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoomRunner
+{
+    class Coin : Animation
+    {
+        public Rectangle rectangle;
+        public Texture2D texture;
+
+
+        public Coin(Rectangle rectangle, Texture2D texture, GraphicsDevice graphics) : base(new string[] { "Coin" })
+        {
+            this.rectangle = rectangle;
+            this.texture = texture;
+            Rectangle[] collectablesRectangleArray = Player.LoadSheet(5, 6, 32, 32);
+
+            AddAnimation("Coin", texture, graphics, 5, Player.LoadSheet(5, 6, 32, 32));
+        }
+
+        public new void Update()
+        {
+            base.Update();
+        }
+
+    }
+}
