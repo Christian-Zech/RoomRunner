8c885473e20f191bdc466faf2c4a1576cec5011d
Owen Isenhart <isenhart.owen@gmail.com>
2023-03-27 10:54:41 -0500

    fixed bug where you couldn't play without a hat

--- RoomRunner/RoomRunner/Player.cs
+++ RoomRunner/RoomRunner/Player.cs
@@ -170,7 +170,8 @@ namespace RoomRunner
         {
             if (Idle) return;
             sb.Draw(CurrentTexture, PlayerRectangle, Color.White);
-            sb.Draw(Hats[currentHat], HatRectangle, Color.White);
+            if (currentHat != PlayerHats.None)
+                sb.Draw(Hats[currentHat], HatRectangle, Color.White);
         }
 
         public static Rectangle[] LoadSheet(int width, int height, int Swidth, int Sheight, int extraWhitespace = 0)
